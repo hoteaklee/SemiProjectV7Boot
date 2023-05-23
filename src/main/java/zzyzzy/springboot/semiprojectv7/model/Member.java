027b067efe602e27044ff1cc5c2d4394e3045b96
hoteaklee <cc18ht@naver.com>
2023-05-23 18:12:00 +0900

    회원가입시 회원 정보를 위한 모델 클래스 작성 (Transient 애노테이션 추가)

--- src/main/java/zzyzzy/springboot/semiprojectv7/model/Member.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/model/Member.java
@@ -1,9 +1,11 @@
 package zzyzzy.springboot.semiprojectv7.model;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Entity
@@ -20,15 +22,46 @@ public class Member {
     private String name;
     private String jumin1;
     private String jumin2;
+    @NotBlank(message = "아이디 입력하셔야 됩니다. ")
+    @Length(min=6, max = 16, message = "아이디는 최소 6자, 최대16자이어야 합니다.")
     private String userid;
+    @NotBlank(message = "비빌번호 입력하셔야 됩니다. ")
+    @Length(min=6, max = 16, message = "비밀번호는 최소 6자, 최대16자이어야 합니다.")
     private String passwd;
+
     private String zipcode;
+    @NotBlank(message = "주소 입력하셔야 됩니다. ")
     private String addr1;
+    @NotBlank(message = "주소 입력하셔야 됩니다. ")
     private String addr2;
+    @NotBlank(message = "이메일  입력하셔야 됩니다. ")
     private String email;
     private String phone;
     @CreatedDate
     @Column(insertable = false, updatable = false)
     private LocalDateTime regdate;
+
+    //----------------------
+    @Transient  // 필드 영속성을 지원하지 않음 ( 디비와 연결되지 않음 - 테이블에는 존재하지 않음)
+    @NotBlank(message = "우편번호 입력하셔야 됩니다. ")
+    private String zip1;
+    @Transient
+    @NotBlank(message = "우편번호 입력하셔야 됩니다. ")
+    private String zip2;
+
+    @Transient
+    @NotBlank(message = "이메일 입력하셔야 됩니다. ")
+    private String email1;
+
+    @Transient
+    @NotBlank(message = "이메일 입력하셔야 됩니다. ")
+    private String email2;
+
+    @Transient
+    @NotBlank(message = "전화번호 입력하셔야 됩니다. ")
+    private String tel2;
+    @Transient
+    @NotBlank(message = "전화번호 입력하셔야 됩니다. ")
+    private String tel3;
 
 }
