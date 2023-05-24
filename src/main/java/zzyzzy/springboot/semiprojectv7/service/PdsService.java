d29cd28c42c3f8e67d866f10e72e6c62f8477eec
hoteaklee <cc18ht@naver.com>
2023-05-24 18:15:49 +0900

    자료실 - 서비스 구현

---
+++ src/main/java/zzyzzy/springboot/semiprojectv7/service/PdsService.java
@@ -0,0 +1,10 @@
+package zzyzzy.springboot.semiprojectv7.service;
+
+import org.springframework.web.multipart.MultipartFile;
+import zzyzzy.springboot.semiprojectv7.model.Pds;
+
+public interface PdsService {
+
+    int newPds(Pds pds);
+    boolean newPdsAttach(MultipartFile attach, int pno);
+}
---
+++ src/main/java/zzyzzy/springboot/semiprojectv7/service/PdsServiceImpl.java
@@ -0,0 +1,27 @@
+package zzyzzy.springboot.semiprojectv7.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import zzyzzy.springboot.semiprojectv7.dao.PdsDAO;
+import zzyzzy.springboot.semiprojectv7.model.Pds;
+import zzyzzy.springboot.semiprojectv7.utils.PdsUtils;
+
+@Service("pdssrv")
+public class PdsServiceImpl implements PdsService{
+
+    @Autowired PdsDAO pdsdao;
+    @Autowired
+    PdsUtils pdsUtils;
+
+    @Override
+    public int newPds(Pds pds) {
+        pds.setUuid(pdsUtils.makeUUID());
+        return pdsdao.insertPds(pds);
+    }
+
+    @Override
+    public boolean newPdsAttach(MultipartFile attach, int pno) {
+        return true;// 일단 잘넘어가도록 true해줌
+    }
+}
