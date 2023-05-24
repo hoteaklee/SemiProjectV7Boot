bfd15297f7ec895a7c40f06175191f2d66a3be5a
hoteaklee <cc18ht@naver.com>
2023-05-24 13:10:11 +0900

    첨부파일 다운로드 구현 1 브라우저로 파일 전송하기 (ResponesEntity) (파일럿 프로그램)

--- src/main/java/zzyzzy/springboot/semiprojectv7/pilot/PilotController.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/pilot/PilotController.java
@@ -1,14 +1,23 @@
 package zzyzzy.springboot.semiprojectv7.pilot;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -74,4 +83,35 @@ public class PilotController {
     public String list(){
         return "pilot/list";
     }
+
+    @GetMapping("/down")
+    public ResponseEntity<Resource> down(int pno) throws IOException {
+
+        // 다운로드 전송할 파일 식별
+        String savePath = "C:/Java/bootUpload/";
+        String fname = "";
+        if (pno == 1) fname += "riho.png"; // 한글파일
+        else if (pno == 2) fname += "write.html";
+        else if (pno == 3) fname += "newOne.zip";
+
+        // 파일이름에 한글이 포함된 경우 적절한 인코딩 작업 수행
+        fname = UriUtils.encode(fname, StandardCharsets.UTF_8);
+
+        // 다운로드할 파일 객체 생석
+        UrlResource resource = new UrlResource("file:" + (savePath + fname));
+
+        // MIME 타입 지정
+        // 브라우저에 다운로드할 파일에 대한 정보 제공
+        HttpHeaders header = new HttpHeaders();
+        header.add("Content-Type", Files.probeContentType(Paths.get(savePath+fname)));
+        header.add("Content-Disposition",
+                "attachment; filename=" + fname + "");
+
+        // 브라우저 파일 전송하기
+        return ResponseEntity.ok().headers(header).body(resource);
+
+    }
+
+
+
 }
