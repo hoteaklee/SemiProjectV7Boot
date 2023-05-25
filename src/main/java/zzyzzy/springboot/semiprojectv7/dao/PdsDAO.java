4bb1e78b1d686ab45cdc9b291ad3bec3992ccfc2
hoteaklee <cc18ht@naver.com>
2023-05-25 10:57:27 +0900

    자료실 - PdsDao 수정 PdsAttach 저장 구현

--- src/main/java/zzyzzy/springboot/semiprojectv7/dao/PdsDAO.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/dao/PdsDAO.java
@@ -1,7 +1,10 @@
 package zzyzzy.springboot.semiprojectv7.dao;
 
 import zzyzzy.springboot.semiprojectv7.model.Pds;
+import zzyzzy.springboot.semiprojectv7.model.PdsAttach;
 
 public interface PdsDAO {
     int insertPds(Pds pds);
+
+    int insertPdsAttach(PdsAttach pa);
 }
