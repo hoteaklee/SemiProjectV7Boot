a4d42d86773da6ed55f5ffaaa0b7eae8278a059a
hoteaklee <cc18ht@naver.com>
2023-05-26 16:09:14 +0900

    자료실 댓글 - 댓글쓰기 repository 구현

--- src/main/java/zzyzzy/springboot/semiprojectv7/repository/PdsReplyRepository.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/repository/PdsReplyRepository.java
@@ -1,8 +1,10 @@
 package zzyzzy.springboot.semiprojectv7.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import zzyzzy.springboot.semiprojectv7.model.PdsReply;
 
 import java.util.List;
@@ -10,4 +12,9 @@ import java.util.List;
 public interface PdsReplyRepository extends JpaRepository<PdsReply, Long> {
     //@Query("from PdsReply where pno = :pno order by refno asc")
     List<PdsReply> findByPnoOrderByRefnoAscRegdateAsc(int pno);
+
+    @Modifying
+    @Transactional
+    @Query("update PdsReply set refno = :rpno where rpno = :rpno")
+    void updateRefno(@Param("rpno") int rpno);
 }
