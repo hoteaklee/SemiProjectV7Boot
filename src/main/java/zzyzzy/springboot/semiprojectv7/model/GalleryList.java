ace91a98dc48cd1e600dbf267a989a9f88397a2d
hoteaklee <cc18ht@naver.com>
2023-06-08 12:02:21 +0900

    갤러리 - Gallery list projection

---
+++ src/main/java/zzyzzy/springboot/semiprojectv7/model/GalleryList.java
@@ -0,0 +1,9 @@
+package zzyzzy.springboot.semiprojectv7.model;
+
+// a.fname, g.gno, g.title, g.userid, g.regdate, g.thumbs, g.views 를 가져옴 밑에걸 2개 쓰면
+public interface GalleryList {
+    String getFname();
+    Gallery getGallery();
+
+
+}
