91f7eb21564f3b24ae9eddc9285a253de32163e6
hoteaklee <cc18ht@naver.com>
2023-06-08 11:52:59 +0900

    갤러리 - Gallery list dao

--- src/main/java/zzyzzy/springboot/semiprojectv7/dao/GalleryDAOImpl.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/dao/GalleryDAOImpl.java
@@ -1,11 +1,18 @@
 package zzyzzy.springboot.semiprojectv7.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import zzyzzy.springboot.semiprojectv7.model.GalAttach;
 import zzyzzy.springboot.semiprojectv7.model.Gallery;
 import zzyzzy.springboot.semiprojectv7.repository.GalleryRepository;
 import zzyzzy.springboot.semiprojectv7.repository.GalleryaRepository;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Repository("galdao")
@@ -30,5 +37,18 @@ public class GalleryDAOImpl implements GalleryDAO{
     @Override
     public int insertGalAttach(GalAttach ga) {
         return galleryaRepository.save(ga).getGno();
+    }
+
+    @Override
+    public Map<String, Object> selectGallery(Integer cpg) {
+        //페이징 시 정렬 순서 지정
+        Pageable paging = PageRequest.of(cpg,25, Sort.Direction.DESC,"gno");
+
+        Map<String, Object> gals = new HashMap<>();
+        // Gallery와 GalAttach를 조인해서 리스트로 가져옴
+        gals.put("gallist",galleryRepository.findAllBy(paging).getContent());
+        gals.put("cntpg",galleryRepository.findAllBy(paging).getTotalPages());
+
+        return gals;
     }
 }
