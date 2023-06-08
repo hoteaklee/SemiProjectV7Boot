6ef38a15ba11c9e050bab5ab816b96fa85fb8d5c
hoteaklee <cc18ht@naver.com>
2023-06-08 13:17:52 +0900

    갤러리 - Gallery view controller

--- src/main/java/zzyzzy/springboot/semiprojectv7/controller/GalleryController.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/controller/GalleryController.java
@@ -57,4 +57,14 @@ public class GalleryController {
         return viewPage;
     }
 
+    @GetMapping("/view")
+    public ModelAndView view(int gno) {
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("gal", galsrv.readOneGallery(gno));
+        mv.setViewName("gallery/view");
+
+        return mv;
+    }
+
 }
