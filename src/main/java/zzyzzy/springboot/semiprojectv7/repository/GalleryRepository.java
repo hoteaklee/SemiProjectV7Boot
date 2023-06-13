da238a842be03c989eed153f509bd4d6ffefce9a
hoteaklee <cc18ht@naver.com>
2023-06-13 18:14:22 +0900

    갤러리 - Gallery list html 수정

--- src/main/java/zzyzzy/springboot/semiprojectv7/repository/GalleryRepository.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/repository/GalleryRepository.java
@@ -1,11 +1,14 @@
 package zzyzzy.springboot.semiprojectv7.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import zzyzzy.springboot.semiprojectv7.model.Gallery;
+import zzyzzy.springboot.semiprojectv7.model.GalleryList;
 import zzyzzy.springboot.semiprojectv7.model.Pds;
 
 public interface GalleryRepository extends JpaRepository<Gallery, Long> {
@@ -13,5 +16,7 @@ public interface GalleryRepository extends JpaRepository<Gallery, Long> {
 //    @Transactional
 //    @Query("update Gallery set views = views +1 where gno = :gno")
 //    void countViewById(@Param("pno") long gno);  //@Param("gno") => :gno
+
+
 
 }
